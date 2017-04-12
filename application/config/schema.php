c5f9e24e86cd17671cab700e5a1baf3f06262fd4
shivasdst <shivably04sdst@gmail.com>
2017-04-12 12:48:11 +0530

    changed schema

--- application/config/schema.php
+++ application/config/schema.php
@@ -1,43 +1,17 @@
 <?php
 
-define('DB_SCHEMA', 'CREATE DATABASE IF NOT EXISTS :db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci');
+define('DB_SCHEMA', 'CREATE DATABASE IF NOT EXISTS :db CHARACTER SET utf8 COLLATE utf8_general_ci');
 
 define('METADATA_TABLE_L1_SCHEMA', 'CREATE TABLE `' . METADATA_TABLE_L1 . '` (
-  `albumID` varchar(50) NOT NULL,
-  `description` text DEFAULT NULL,
-  PRIMARY KEY (`albumID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4');
+  `name` varchar(500) NOT NULL,
+  `sal` varchar(50) DEFAULT NULL,
+  `rollno` varchar(50) DEFAULT NULL,
+  `batch_details` varchar(500) DEFAULT NULL,
+  `year_awarded` int(10) DEFAULT NULL,
+  `info` varchar(2000) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8');
 
-define('METADATA_TABLE_L2_SCHEMA', 'CREATE TABLE `' . METADATA_TABLE_L2 . '` (
-  `id` varchar(50) NOT NULL,
-  `albumID` varchar(50) NOT NULL,
-  `description` text DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4');
 
-define('METADATA_TABLE_L3_SCHEMA', 'CREATE TABLE IF NOT EXISTS `' . METADATA_TABLE_L3 . '` (
-`name` varchar(1000), 
-`email` varchar(100), 
-`profession` varchar(500), 
-`password` varchar(100), 
-`affiliation` varchar(2000), 
-`misc` varchar(1000), 
-`isverified` varchar(1), 
-`visitcount` int(5), 
-`hash` varchar(64), 
-`timestamp` varchar(20), 
-`userid` int(6) AUTO_INCREMENT, PRIMARY KEY(userid)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4');
-
-define('METADATA_TABLE_L4_SCHEMA', 'CREATE TABLE IF NOT EXISTS `' . METADATA_TABLE_L4 . '` (
-`hash` varchar(100), 
-`email` varchar(100), 
-`name` varchar(1000), 
-`password` varchar(100), 
-`timestamp` varchar(100), 
-`resetid` int(6) AUTO_INCREMENT, PRIMARY KEY (resetid)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4');
-
-define('CHAR_ENCODING_SCHEMA', 'SET NAMES utf8mb4');
+define('CHAR_ENCODING_SCHEMA', 'SET NAMES utf8');
 
 ?>
