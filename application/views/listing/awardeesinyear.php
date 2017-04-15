a201d67a07ad48a03013dc6d404583ee6ad4cc52
shivasdst <shivably04sdst@gmail.com>
2017-04-15 16:54:45 +0530

    removed masonry layout

--- application/views/listing/awardeesinyear.php
+++ application/views/listing/awardeesinyear.php
@@ -1,15 +1,12 @@
-<div id="grid" class="container-fluid">
-    <div id="posts">
+<div id="grid" class="container">
+    <div id="row">
 <?php foreach ($data as $row) { ?>
-        <div class="post">
+        <div class="col-md-2">
             <a href="<?=BASE_URL?>Awardees/<?=$row->year_awarded?>/<?=str_replace(' ', '_', $row->name)?>" title="View Awardee">
                 <div class="fixOverlayDiv">
                     <img class="img-responsive" src="<?=$viewHelper->includeThumbnail($row->name,$row->year_awarded)?>">
-                    <div class="OverlayText"><small><?= $row->sal?> <?= $row->name?></small> <span class="link"><i class="fa fa-link"></i></span></div>
+                    <div class="OverlayText"><small><?= $row->sal?> <?= $row->name?></small></div>
                 </div>
-                <p class="image-desc">
-                    <strong><?=$row->info?></strong>
-                </p>
             </a>
         </div>
 <?php } ?>
