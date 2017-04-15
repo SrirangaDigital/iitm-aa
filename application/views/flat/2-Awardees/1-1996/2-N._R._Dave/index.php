59bd2e2d09763b635ffa3b75f036772c49952e8e
shivasdst <shivably04sdst@gmail.com>
2017-04-15 15:38:09 +0530

    Awardees page implemented

---
+++ application/views/flat/2-Awardees/1-1996/2-N._R._Dave/index.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-9 main">
+	    	<h1>Dr. N. R. Dave</h1>
+	    	<h2></h2>
+            <img src="<?=FLAT_IMAGE_URL?>1996/N._R._Dave.jpg" class="profile-thumb" alt="Dr. N. R. Dave" />
+            <p class="affiliation">
+            	<span>1965 - BTech - Civil Engineering</span><br />
+				<span>Owner, NRDAVE</span>
+            </p>
+			<p>Shri Dave has employed an independent professional approach to improve the quality of the physically challenged.</p>
+			<p>He acquired his degree of Bachelor of Technology in Civil Engineering at this Institute. After work for two years in other companies, he started his own construction and consultancy firm. During the decades that have gone by, he built many residential, commercial and industrial buildings and restored many old and monumental buildings to their original glory. He is a Class-l surveyor of the Corporation of Madras, an approved Engineer for certifying the soundness of public buildings, Wealth-Tax valuer, member to the Concrete Association of India Valuers Association and Vastu Sastra Circle. He has also acquitted himself honorably as a past District Governor of Lions Club International and Under-Sheriff of Madras. He is a life member of the Institution of Engineers and has written papers on social aspects of Civil Engineering, Cracks, Repairs and Maintenance of buildings.</p>
+			<p>Recipient of many awards for social work, Shri Dave combines the creativity of an Engineering Scientist with the compassion of a Social Worker. He is the founder and Managing Trustee of Lions Hearing Aid Bank, Lions Bank Trust, and Kanya Sharan, a home for destitute girls. He is also the founder of Lions Edifice for Service Trust, Lions Model Eye Bank, Heart Foundation and Lions Foundation for the Blind. In short, Shri Dave represents the social conscience of the community of Engineers.</p>
+	    </div>		
+	    <div class="col-md-3 sidebar">
+			<?php require_once('application/views/generalSidebar.php');?>
+    	</div>    
+    </div>
+</div>    
