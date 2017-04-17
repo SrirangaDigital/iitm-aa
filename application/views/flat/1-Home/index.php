05cde7eb653a400c66a4f1d2aa1cf8af119f2880
arjunkashyap <arjun.kashyap@yahoo.co.in>
2017-04-17 20:51:31 +0530

    UI fixes - finalized

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -3,29 +3,17 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-md-2">
-            <div class="intro logos">
-                <p>
-                    <img src="<?=PUBLIC_URL?>images/logo.png" alt="IAS Logo" /><br />
-                    <img src="<?=PUBLIC_URL?>images/stock/IAR_logo.png" alt="IISER Bhopal Logo" />
-                </p>
-            </div>
-        </div>
-        <div class="col-md-10">
+        <div class="col-md-12">
             <div class="intro">
                 <p>The Distinguished Alumnus Awards are presented annually by the Institute. Since the inception of the awards in 1996, 147 alumni have been selected for the award.</p>
                 <p>Office of the Alumni Relations will receive nominations for the Distinguished Alumnus Awards. The selection is done by a committee appointed by the Director. Around 8 to 10 alumni are selected for the awards each year.The awards are presented during the Institute Day held around mid April each year.</strong></p>
+                <p><img src="<?=PUBLIC_URL?>images/stock/IAR_logo.png" alt="IISER Bhopal Logo" /></p>
+                <p>View Distinguished Alumin Awardees</p>
+                <a class="selection-btn" href="<?=BASE_URL?>listing/byyear">by Year</a>  
+                <a class="selection-btn" href="<?=BASE_URL?>listing/byname">by Name</a>        
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-6">
-            <a class="btn-lg btn-primary btn-block text-center" href="<?=BASE_URL?>listing/byyear">Display Alumnus by Year</a>  
-        </div>        
-        <div class="col-md-6">
-            <a class="btn-lg btn-primary btn-block text-center" href="<?=BASE_URL?>listing/byname">Display Alumnus by Name</a>        
-        </div>
-    </div>    
 </div>
 
 
