062569f64b6b3b9a579f6a697f96619eba085ef1
suresh07 <soori_rozee@yahoo.com>
2017-04-17 19:40:08 +0530

    UI Fix

--- application/views/listing/names.php
+++ application/views/listing/names.php
@@ -1,7 +1,7 @@
-<div id="grid" class="container">
-    <div id="row">
+<div id="grid" class="container-fluid">
+    <div id="posts">
 <?php foreach ($data as $row) { ?>
-        <div class="col-md-2">
+        <div class="post">
             <a href="<?=BASE_URL?>Awardees/<?=$row->year_awarded?>/<?=str_replace(' ', '_', $row->name)?>" title="View Awardee">
                 <div class="fixOverlayDiv">
                     <img class="img-responsive" src="<?=$viewHelper->includeThumbnail($row->name,$row->year_awarded)?>">
