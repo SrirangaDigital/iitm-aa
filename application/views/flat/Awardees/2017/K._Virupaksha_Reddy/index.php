69ac980a8d8379ff0b99910ab90d886f2e898ecd
suresh07 <soori_rozee@yahoo.com>
2017-05-03 11:02:58 +0530

    2017 awardees flat pages

---
+++ application/views/flat/Awardees/2017/K._Virupaksha_Reddy/index.php
@@ -0,0 +1,17 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 main">
+			<h1>Dr. K. Virupaksha Reddy</h1>
+			<h2>Awarded: 2017</h2>
+			<img src="<?=PROFILE_IMAGE_URL?>2017/K._Virupaksha_Reddy.jpg" class="profile-thumb" alt="profile picture" />
+			<p class="affiliation">
+				<span>Founder/President, PriTel Inc., USA</span><br /><span>1972/MSc/CY</span>
+			</p>
+			<p>Dr. K. Virupaksha Reddy graduated with a Master’s degree in Chemistry from IIT Madras in 1972, after obtaining his Bachelor’s degree from the Government Arts College in Anantapur. During his Ph.D. in the Physical Chemistry Department at the University of Wisconsin-Madison, he developed pulsed and CW lasers to study rates of chemical reactions using intra-cavity laser excitation schemes. This work was widely cited, gaining appreciation from stalwarts across the field.</p>
+			<p>Dr. Reddy joined the Amoco Research Center in 1981 after brief stints at Allied Chemicals and McDonnell Douglas. It was at Amoco that his innovative talents came to the forefront and paved the way to the invention of a primer-less painting technology for composite panels by flash evaporation with nanosecond duration ultra-violet lasers. Significantly, he also invented ultra-stable direct ablation of Teflon using the same ultra-violet laser. He had the technical insight to identify and build, in 1992, the world’s first commercial femtosecond fiber laser in a compact box.</p>
+			<p>In 1995, Dr. Reddy founded PriTel, Inc., to commercialize Femtosecond Fiber Lasers (FFL), in close collaboration with the Optoelectronics Research Centre at the University of Southampton, UK. In 1996, he ingeniously combined optical technologies with RF electronics to develop a mechanism to provide a stable train of optical pulses at repetition rates of up to 1200 Gbps. Over the course of 22 years, Dr. Reddy continued to innovate and maintain his focus on this niche hardware market. PriTel, Inc., now has a global customer base with 80% of its revenue derived from outside of the USA.</p>
+			<p>Dr. Reddy and his wife, Sarveswari, support the institutions that laid the foundation for his success through contributions to IIT Madras, the University of Wisconsin-Madison, and the University of Southampton in the form of endowed Chairs, endowed scholarships, lecture series, and numerous other philanthropic mechanisms. Dr. Reddy has also served as the President of the IIT Madras Alumni Association of North America from 2013 to 2015.</p>
+			<p>For his revolutionary work in the area of Optoelectronics, and for his business leadership and acumen, IIT Madras and its alumni are proud to confer this award upon Dr. K. Virupaksha Reddy.</p>
+		</div>
+	</div>
+</div>
