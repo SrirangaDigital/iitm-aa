319cd37d1c583914186fe77fbac11a1c38b46773
suresh07 <soori_rozee@yahoo.com>
2017-05-04 17:30:53 +0530

    Overlay text with fa link icon removed

--- application/views/listing/names.php
+++ application/views/listing/names.php
@@ -5,12 +5,12 @@
             <a href="<?=BASE_URL?>Awardees/<?=$row->year_awarded?>/<?=str_replace(' ', '_', $row->name)?>" title="View Awardee">
                 <div class="fixOverlayDiv">
                     <img class="img-responsive" src="<?=$viewHelper->includeAvatarPhotos($row->name,$row->year_awarded)?>">
-                    <div class="OverlayText"><span class="link"><i class="fa fa-link"></i></span></div>
                 </div>
                 <p class="image-desc">
                     <strong><?=$row->info?></strong>
                 </p>
             </a>
+            <div class="trule">&nbsp;</div>
         </div>
 <?php } ?>
     </div>
